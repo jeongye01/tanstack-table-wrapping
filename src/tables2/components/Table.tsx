38350eae046dd8e648d6b211ca819d96edab8e7f
yewon_jeong <yewon_jeong@tmax.co.kr>
2024-12-16 20:25:48 +0900

    feat: containter width 보다 작아지면 reszing 방지

--- src/tables2/components/Table.tsx
+++ src/tables2/components/Table.tsx
@@ -1,169 +1,208 @@
 import {
-   flexRender,
-   getCoreRowModel,
-   getSortedRowModel,
-   type SortingState,
-   type TableOptions,
-   useReactTable,
-} from '@tanstack/react-table';
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  type SortingState,
+  type TableOptions,
+  useReactTable,
+} from "@tanstack/react-table";
 
-import { type CSSProperties, useEffect, useRef, useState } from 'react';
+import { type CSSProperties, useState } from "react";
 
-import { SortingButton } from './Header/SortingButton';
-import { useResizableColumns } from './hooks/useResizableColumns';
+import { SortingButton } from "./Header/SortingButton";
+import { useResizableColumns } from "./hooks/useResizableColumns";
 export function passiveEventSupported() {
-   let supported = false;
-   try {
-      const options = {
-         get passive() {
-            supported = true;
-            return false;
-         },
-      };
-
-      const noop = () => {};
-
-      window.addEventListener('test', noop, options);
-      window.removeEventListener('test', noop);
-   } catch {
-      supported = false;
-   }
-
-   return supported;
-}
-
-interface TableProps<TData> extends Pick<TableOptions<TData>, 'data' | 'columns'> {
-   option?: {
-      size?: {
-         width?: CSSProperties['width'];
-         height?: CSSProperties['height'];
-         minWidth?: CSSProperties['minWidth'];
-         minHeight?: CSSProperties['minHeight'];
-         maxWidth?: CSSProperties['maxWidth'];
-         maxHeight?: CSSProperties['maxHeight'];
-      };
-   };
-}
-
-export const Table = <TData,>({ data, columns: initColumns, option }: TableProps<TData>) => {
-   const { size } = option || {};
-   const [sorting, setSorting] = useState<SortingState>([]);
-
-   const { containerRef, columns, containerWidth, columnSizeMap, setColumnSizeMap } = useResizableColumns({
-      initColumns,
-   });
-
-   const table = useReactTable({
-      data,
-      columns,
-      columnResizeMode: 'onChange',
-      columnResizeDirection: 'ltr',
-      getCoreRowModel: getCoreRowModel(),
-      getSortedRowModel: getSortedRowModel(),
-      onSortingChange: setSorting,
-      state: {
-         sorting,
+  let supported = false;
+  try {
+    const options = {
+      get passive() {
+        supported = true;
+        return false;
       },
-   });
+    };
 
-   const handleResize = (columnId: string, newSize: number) => {
-      console.log(newSize, columnId);
-      setColumnSizeMap({
-         ...columnSizeMap,
-         [columnId]: newSize,
-      });
-   };
-   console.log(columnSizeMap);
+    const noop = () => {};
 
-   const columnResizeHandler = (_contextDocument?: Document) => {
-      const contextDocument = _contextDocument || typeof document !== 'undefined' ? document : null;
+    window.addEventListener("test", noop, options);
+    window.removeEventListener("test", noop);
+  } catch {
+    supported = false;
+  }
 
-      return (event: React.MouseEvent, columnId: string) => {
-         const startX = event.clientX; // 초기 마우스 X 위치
-         const startSize = columnSizeMap[columnId]; // 초기 열 크기
+  return supported;
+}
 
-         const handleMouseMove = (moveEvent: MouseEvent) => {
-            const deltaX = moveEvent.clientX - startX;
-            const deltaPercentage = Math.max(deltaX / startSize, -0.999999);
-            const newSize = Math.round(Math.max(startSize + startSize * deltaPercentage, 0) * 100) / 100;
-            handleResize(columnId, newSize);
-         };
-         const handleMouseUp = () => {
-            contextDocument?.removeEventListener('mousemove', handleMouseMove);
-            contextDocument?.removeEventListener('mouseup', handleMouseUp);
-         };
-         const passiveIfSupported = passiveEventSupported() ? { passive: false } : false;
+interface TableProps<TData>
+  extends Pick<TableOptions<TData>, "data" | "columns"> {
+  option?: {
+    size?: {
+      width?: CSSProperties["width"];
+      height?: CSSProperties["height"];
+      minWidth?: CSSProperties["minWidth"];
+      minHeight?: CSSProperties["minHeight"];
+      maxWidth?: CSSProperties["maxWidth"];
+      maxHeight?: CSSProperties["maxHeight"];
+    };
+  };
+}
 
-         contextDocument?.addEventListener('mousemove', handleMouseMove, passiveIfSupported);
-         contextDocument?.addEventListener('mouseup', handleMouseUp, passiveIfSupported);
+export const Table = <TData,>({
+  data,
+  columns: initColumns,
+  option,
+}: TableProps<TData>) => {
+  const { size } = option || {};
+  const [sorting, setSorting] = useState<SortingState>([]);
+
+  const {
+    containerRef,
+    columns,
+    containerWidth,
+    columnSizeMap,
+    setColumnSizeMap,
+  } = useResizableColumns({
+    initColumns,
+  });
+
+  const table = useReactTable({
+    data,
+    columns,
+    columnResizeMode: "onChange",
+    columnResizeDirection: "ltr",
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onSortingChange: setSorting,
+    state: {
+      sorting,
+    },
+  });
+
+  const handleResize = (columnId: string, newSize: number) => {
+    const otherColumnsWidth = Object.entries(columnSizeMap)
+      .filter(([key]) => key !== columnId)
+      .reduce((sum, [, width]) => sum + width, 0);
+
+    const minAllowedWidth = containerWidth || 0;
+    const remainingWidth = minAllowedWidth - otherColumnsWidth;
+
+    const adjustedSize = Math.max(newSize, remainingWidth);
+
+    setColumnSizeMap({
+      ...columnSizeMap,
+      [columnId]: adjustedSize,
+    });
+  };
+
+  const columnResizeHandler = (_contextDocument?: Document) => {
+    const contextDocument =
+      _contextDocument || typeof document !== "undefined" ? document : null;
+
+    return (event: React.MouseEvent, columnId: string) => {
+      const startX = event.clientX;
+      const startSize = columnSizeMap[columnId];
+
+      const handleMouseMove = (moveEvent: MouseEvent) => {
+        const deltaX = moveEvent.clientX - startX;
+        const newSize = startSize + deltaX;
+        handleResize(columnId, newSize);
       };
-   };
-   return (
-      <div
-         ref={containerRef}
-         className="dbmaster-table-container dbmaster-table-scrollbar"
-         style={{
-            height: size?.height ?? '100%',
-            minHeight: size?.minHeight,
-            maxHeight: size?.maxHeight,
-            width: size?.width ?? '100%',
-            maxWidth: size?.maxWidth,
-            minWidth: size?.minWidth,
-         }}
+      const handleMouseUp = () => {
+        contextDocument?.removeEventListener("mousemove", handleMouseMove);
+        contextDocument?.removeEventListener("mouseup", handleMouseUp);
+      };
+      const passiveIfSupported = passiveEventSupported()
+        ? { passive: false }
+        : false;
+
+      contextDocument?.addEventListener(
+        "mousemove",
+        handleMouseMove,
+        passiveIfSupported
+      );
+      contextDocument?.addEventListener(
+        "mouseup",
+        handleMouseUp,
+        passiveIfSupported
+      );
+    };
+  };
+  return (
+    <div
+      ref={containerRef}
+      className="dbmaster-table-container dbmaster-table-scrollbar"
+      style={{
+        height: size?.height ?? "100%",
+        minHeight: size?.minHeight,
+        maxHeight: size?.maxHeight,
+        width: size?.width ?? "100%",
+        maxWidth: size?.maxWidth,
+        minWidth: size?.minWidth,
+      }}
+    >
+      <table
+        className="dbmaster-table"
+        style={{ opacity: containerWidth ? 1 : 0 }}
       >
-         <table className="dbmaster-table" style={{ opacity: containerWidth ? 1 : 0 }}>
-            <thead className="dbmaster-thead">
-               {table.getHeaderGroups().map(headerGroup => (
-                  <tr key={headerGroup.id} className="dbmaster-tr">
-                     {headerGroup.headers.map(header => (
-                        <th
-                           key={header.id}
-                           style={{
-                              width: columnSizeMap?.[header.id],
-                           }}
-                           data-column-id={header.id}
-                           className="dbmaster-th"
-                        >
-                           {flexRender(header.column.columnDef.header, header.getContext())}
-                           {header.column.getCanSort() && (
-                              <SortingButton
-                                 sortDirection={header.column.getIsSorted() || undefined}
-                                 onClick={() => header.column.toggleSorting()}
-                              />
-                           )}
-                           {header.column.getCanResize() && (
-                              <div
-                                 {...{
-                                    onDoubleClick: () => header.column.resetSize(),
-                                    onMouseDown: event => columnResizeHandler()(event, header.id), // 반환된 핸들러 호출
-                                    className: `
+        <thead className="dbmaster-thead">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className="dbmaster-tr">
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  style={{
+                    width: columnSizeMap?.[header.id],
+                  }}
+                  data-column-id={header.id}
+                  className="dbmaster-th"
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                  {header.column.getCanSort() && (
+                    <SortingButton
+                      sortDirection={header.column.getIsSorted() || undefined}
+                      onClick={() => header.column.toggleSorting()}
+                    />
+                  )}
+                  {header.column.getCanResize() && (
+                    <div
+                      {...{
+                        onDoubleClick: () => header.column.resetSize(),
+                        onMouseDown: (event) =>
+                          columnResizeHandler()(event, header.id), // 반환된 핸들러 호출
+                        className: `
                                     dbmaster-column-resizer-bar
-                                    ${header.column.getIsResizing() ? 'isResizing' : ''}`,
-                                 }}
-                              />
-                           )}
-                        </th>
-                     ))}
-                  </tr>
-               ))}
-            </thead>
-            <tbody className="dbmaster-tbody">
-               {table.getRowModel().rows.map(row => (
-                  <tr key={row.id} className="dbmaster-tr">
-                     {row.getVisibleCells().map(cell => (
-                        <td
-                           key={cell.id}
-                           data-column-id={cell.id}
-                           style={{ width: columnSizeMap?.[cell.column.id] }}
-                           className="dbmaster-td"
-                        >
-                           {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                        </td>
-                     ))}
-                  </tr>
-               ))}
-            </tbody>
-         </table>
-      </div>
-   );
+                                    ${
+                                      header.column.getIsResizing()
+                                        ? "isResizing"
+                                        : ""
+                                    }`,
+                      }}
+                    />
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody className="dbmaster-tbody">
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id} className="dbmaster-tr">
+              {row.getVisibleCells().map((cell) => (
+                <td
+                  key={cell.id}
+                  data-column-id={cell.id}
+                  style={{ width: columnSizeMap?.[cell.column.id] }}
+                  className="dbmaster-td"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
--- src/tables2/components/hooks/useResizableColumns.tsx
+++ src/tables2/components/hooks/useResizableColumns.tsx
@@ -1,60 +1,78 @@
-import { TableOptions } from '@tanstack/react-table';
-import { useEffect, useRef, useState } from 'react';
+import { TableOptions } from "@tanstack/react-table";
+import { useEffect, useRef, useState } from "react";
 
 interface UseResizableColumnsOptions<TColumn> {
-   initColumns: TableOptions<TColumn>['columns'];
+  initColumns: TableOptions<TColumn>["columns"];
 }
 
-export function useResizableColumns<TColumn>({ initColumns }: UseResizableColumnsOptions<TColumn>) {
-   const containerRef = useRef<HTMLDivElement>(null);
-   const [containerWidth, setContainerWidth] = useState<number>();
-   const [columns, setColumns] = useState(initColumns);
-   const [columnSizeMap, setColumnSizeMap] = useState<Record<string, number>>({});
+export function useResizableColumns<TColumn>({
+  initColumns,
+}: UseResizableColumnsOptions<TColumn>) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [containerWidth, setContainerWidth] = useState<number>();
+  const [columns, setColumns] = useState(initColumns);
+  const [columnSizeMap, setColumnSizeMap] = useState<Record<string, number>>(
+    {}
+  );
 
-   useEffect(() => {
-      const container = containerRef.current;
+  useEffect(() => {
+    const container = containerRef.current;
 
-      if (!container) return;
+    if (!container) return;
 
-      const observer = new ResizeObserver(entries => {
-         for (const entry of entries) {
-            if (entry.contentRect) {
-               const totalWidth = entry.contentRect.width;
-               const dynamicColumns = initColumns.filter(col => !col.size).length;
-               const staticWidth = columns.reduce((acc, col) => acc + (col.size || 0), 0);
-               const defaultColumnWidth =
-                  dynamicColumns > 0 ? Math.trunc((totalWidth - staticWidth) / dynamicColumns) : 0;
-               const lastColumnWidth = totalWidth - staticWidth - defaultColumnWidth * (dynamicColumns - 1);
-               let left = dynamicColumns; // FIXME: 안티패턴
+    const observer = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.contentRect) {
+          const totalWidth = entry.contentRect.width;
+          const dynamicColumns = initColumns.filter((col) => !col.size).length;
+          const staticWidth = columns.reduce(
+            (acc, col) => acc + (col.size || 0),
+            0
+          );
+          const defaultColumnWidth =
+            dynamicColumns > 0
+              ? Math.trunc((totalWidth - staticWidth) / dynamicColumns)
+              : 0;
+          const lastColumnWidth =
+            totalWidth -
+            staticWidth -
+            defaultColumnWidth * (dynamicColumns - 1);
+          let left = dynamicColumns; // FIXME: 안티패턴
 
-               const newColumns = initColumns.map(col => {
-                  if (col.size) {
-                     return col;
-                  }
-                  left--;
-                  return {
-                     ...col,
-                     size: left === 1 ? lastColumnWidth : defaultColumnWidth,
-                  };
-               });
-               setColumns(newColumns);
-               const sizeMap = newColumns.reduce((acc, column) => {
-                  const key = column.accessorKey; // id 또는 accessorKey 사용
-                  if (key && column.size !== undefined) {
-                     acc[key] = column.size;
-                  }
-                  return acc;
-               }, {} as Record<string, number>);
-               setColumnSizeMap(sizeMap); // 상태 업데이트
-               setContainerWidth(totalWidth);
+          const newColumns = initColumns.map((col) => {
+            if (col.size) {
+              return col;
             }
-         }
-      });
-      observer.observe(container);
-      return () => {
-         observer.disconnect();
-      };
-   }, []);
+            left--;
+            return {
+              ...col,
+              size: left === 1 ? lastColumnWidth : defaultColumnWidth,
+            };
+          });
+          setColumns(newColumns);
+          const sizeMap = newColumns.reduce((acc, column) => {
+            const key = column.accessorKey;
+            if (key && column.size !== undefined) {
+              acc[key] = column.size;
+            }
+            return acc;
+          }, {} as Record<string, number>);
+          setColumnSizeMap(sizeMap);
+          setContainerWidth(totalWidth);
+        }
+      }
+    });
+    observer.observe(container);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
-   return { containerRef, containerWidth, columns, columnSizeMap, setColumnSizeMap };
+  return {
+    containerRef,
+    containerWidth,
+    columns,
+    columnSizeMap,
+    setColumnSizeMap,
+  };
 }
